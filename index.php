c5133d9bfbcb3e50f3c1d5bd66a312ee4901c03f
Tugsjargal <z.togsoo0024@gmail.com>
2021-04-24 21:11:50 +0800

    include ashiglav

--- index.php
+++ index.php
@@ -10,48 +10,10 @@
 </head>
 
 <body>
-    <?php
-    echo "ZT soft";
-    ?>
-    <?php
 
-    function nothooson($too)
-    {
-        if ($too != "") {
-            return $too;
-        }
-        return;
-    }
-
-    function calculate($nom1, $uild, $nom2)
-    {
-        switch($uild) {
-            case "+":
-                return $nom1 + $nom2;
-            break;
-            case "-":
-                return $nom1 - $nom2;
-            break;
-            case "*":
-                return $nom1 * $nom2;
-            break;
-            case "/":
-                return $nom1 / $nom2;
-            break;
-    
-            default:
-                return 'Aldaa';
-        }
-        return 'Mash tom aldaa';
-    }
-
-    if (
-        nothooson($_POST['nomer1'])  && nothooson($_POST['uildel']) && nothooson($_POST['nomer2'])
-    ) {
-        $hariu = calculate($_POST['numer1'], $_POST['uildel'], $_POST['nomer2']);
-    }
-
-    ?>
+<?php
+include 'form.php';
+?>
 
     <div class="container">
         <div class="row">
